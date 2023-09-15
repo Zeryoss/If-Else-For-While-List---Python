2e06e4d0e164018216626d37d13086037c30c207
Vinicius Cordeiro <104448481+Zeryoss@users.noreply.github.com>
2023-09-14 21:09:58 -0300

    Update For.py

--- For.py
+++ For.py
@@ -1,1 +1,22 @@
+# Contando de 1 a 10
+for x in range(1,11,1):
+  print(x)
 
+# Contando de 2 em 2 de 1 a 50
+for x in range(1,51,2):
+  print(x)
+
+# Contando quantos numeros são par nos digitados
+cont=0
+for x in range(4):
+  nr = int(input("Numero: "))
+  if(nr%2 == 0):
+    cont = cont+1
+print("O total de números pares na lista é",cont)
+
+# Somando todos números digitados
+cont=0
+for x in range(6):
+  nr = int(input("Numero: "))
+  cont=cont+nr
+print("A soma de todos os números é",cont)
